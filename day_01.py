3115030febc135f23925a9f83d927bf677932792
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-08-31 01:11:46 +1000

    new file:   day_01.py new file:   day_02.py new file:   day_03.py

---
+++ day_01.py
@@ -0,0 +1,13 @@
+"""
+Write a band name generator program that takes in the city the user is born in and their pet name.
+Display a concatenate name that combines the city name and the pet name for their band name. 
+"""
+print("Welcome to band name generator.")
+# Users city name
+city_name = input("What city where you born in?\n")
+
+# Pet name
+pet_name = input("What is your pet's name?\n")
+
+# Output
+print(f"Your band name could be {city_name} {pet_name}.")
