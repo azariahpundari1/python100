74b826eb60178893010a3a309e15a70f2c5630b6
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-09-24 22:04:41 +1000

    Add first conditional statement

--- day_03.py
+++ day_03.py
@@ -1,3 +1,18 @@
 """
 Day 3 project: Treasure Island
+This is a basic project using conditional statements
 """
+
+print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
+
+direction = input("Left or Right? ").casefold()
+if direction == "Left":
+    # print("test")
+    action = input("Swim or Wait? ").casefold()
+else:
+    print("Game Over.")
+
+
+door = input("Red, Blue or Yellow? ").casefold()
+
+
