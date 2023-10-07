140f37d595ae66499a35e78c1b57fb6ce3c12539
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-10-08 09:54:17 +1000

    Add user input

--- day_04_heads_tails.py
+++ day_04_heads_tails.py
@@ -3,6 +3,9 @@ import random
 # Display question
 print("Heads or Tails?")
 
+# Get user input
+user_input = input(">>> ")
+
 # Get random number between 0 & 1
 random_number = random.randint(0,1)
 if random_number == 0:
