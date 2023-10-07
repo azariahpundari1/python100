c7cb922f4b16452889ca13a1782cee0e561b0a15
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-10-08 09:56:26 +1000

    Display win/lose message

--- day_04_heads_tails.py
+++ day_04_heads_tails.py
@@ -4,12 +4,20 @@ import random
 print("Heads or Tails?")
 
 # Get user input
-user_input = input(">>> ")
+user_input = input(">>> ").capitalize()
 
 # Get random number between 0 & 1
 random_number = random.randint(0,1)
 if random_number == 0:
     print("Tails")
+    if user_input == "Tails":
+        print("You win!")
+    else:
+        print("You lose :(")
 elif random_number == 1:
     print("Heads")
+    if user_input == "Heads":
+        print("You win!")
+    else:
+        print("You lose :(")
 
