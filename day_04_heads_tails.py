81003f720a59fed5a84c3eebe50abef26738b8a5
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-10-08 09:49:27 +1000

    Add random number generator

--- day_04_heads_tails.py
+++ day_04_heads_tails.py
@@ -1,1 +1,12 @@
+import random
+
+# Display question
+print("Heads or Tails?")
+
+# Get random number between 0 & 1
+random_number = random.randint(0,1)
+if random_number == 0:
+    print("Tails")
+elif random_number == 1:
+    print("Heads")
 
