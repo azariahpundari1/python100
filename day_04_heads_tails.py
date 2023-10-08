9203d657e34b8a628359560fca323812c1113fa8
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-10-08 10:17:30 +1000

    Convert code into OOP.

--- day_04_heads_tails.py
+++ day_04_heads_tails.py
@@ -1,24 +1,29 @@
 import random
 
-# Display question
-print("Heads or Tails?")
+def main():
+    # Display question
+    print("Heads or Tails?")
 
-# Get user input
-user_input = input(">>> ").capitalize()
+    # Get user input
+    user_input = input(">>> ").capitalize()
 
-# Get random number between 0 & 1
-random_number = random.randint(0,1)
-if random_number == 0:
-    print("Tails")
-    if user_input == "Tails":
-        print("You win!")
-    else:
-        print("You lose :(")
-elif random_number == 1:
-    print("Heads")
-    if user_input == "Heads":
-        print("You win!")
-    else:
-        print("You lose :(")
+    # Get random number between 0 & 1
+    random_number = random.randint(0,1)
+    if random_number == 0:
+        print("Tails")
+        if user_input == "Tails":
+            print("You win!")
+        else:
+            print("You lose :(")
+    elif random_number == 1:
+        print("Heads")
+        if user_input == "Heads":
+            print("You win!")
+        else:
+            print("You lose :(")
 
-#TODO create functions for concisness
+#TODO Create functions for concisness
+
+
+if __name__ == "__main__":
+    main()
